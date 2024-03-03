491479c4af6b6d68ee9825c77428f59054eaf61d
Emily Ko <emily20000222@keio.jp>
2024-03-04 00:38:29 +0900

    add OOP note

---
+++ basics/OOP/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    Car myCar1 = new Car();
+    Car myCar2 = new Car();
+
+    System.out.println(myCar1.color);
+    System.out.println(myCar1.year);
+    myCar1.drive();
+
+    println();
+    System.out.println(myCar2.color);
+    System.out.println(myCar2.year);
+  }
+}
