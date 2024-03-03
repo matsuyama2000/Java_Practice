b34acd3a2efc517ef26e8ff4accb6ef53c29b9ea
Emily Ko <emily20000222@keio.jp>
2024-03-04 01:00:02 +0900

    add method overriding note

---
+++ basics/methodOverriding/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+
+    Animal normalAnimal = new Animal() ;
+    normalAnimal.speak();
+    
+    Cat littleYellow = new Cat();
+    littleYellow.speak();
+  }
+}
